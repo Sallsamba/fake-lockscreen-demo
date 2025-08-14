7f0f18b71682eef6b0cde2641fc1e03ba3d14759
Sallsamba <sambadioulde98@yahoo.fr>
2025-08-14 02:01:47 +0200

    m

--- lancer-demo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Demonstration Cybersecurite
-echo Lancement de la demonstration d'ecran de verrouillage...
-echo.
-echo RAPPEL: Utilisez Ctrl+Q pour quitter en toute securite
-echo.
-pause
-start "" "Écran de verrouillage Windows.exe"
